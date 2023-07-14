1a2d094d1ce78c73e9700aadb16a3b979721aba8
hao.yang <yh.lwy@qq.com>
2023-07-14 11:42:57 +0800

    fix: 打包配置修改

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: process.env.NODE_ENV === 'prodution' ? "/chart" : "/",
+	base: "/chart",
 	server: {
 		port: 1217,
 		host: "0.0.0.0",
