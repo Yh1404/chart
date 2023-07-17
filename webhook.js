42d0270cd81191e0eb3c6b2bb99d26da0d078d29
hao.yang <yh.lwy@qq.com>
2023-07-17 16:26:24 +0800

    webhook脚本

---
+++ webhook.js
@@ -0,0 +1,36 @@
+const http = require("http")
+const crypto = require("crypto")
+const WEBHOOK_SECRET = "jdaug@*NLDAKJ@ldja)nea!l.apda!Q"
+
+var server = http.createServer(function (request, response) {
+	// 回调函数接收request和response对象,
+	// 获得HTTP请求的method和url:
+
+	if (request.url === "/chart/autodeploy" && request.method === "GET") {
+		let body = []
+		request
+			.on("data", chunk => {
+				body.push(chunk)
+			})
+			.on("end", () => {
+				body = Buffer.concat(body).toString()
+				// at this point, `body` has the entire request body stored in it as a string
+			})
+		const signature = crypto.createHmac("sha256", WEBHOOK_SECRET).update(JSON.stringify(body)).digest("hex")
+		if (request.headers["x-hub-signature-256"] === `sha256=${signature}`) {
+			console.log("success")
+			response.writeHead(200)
+			response.end("success")
+		} else {
+			response.writeHead(401)
+			response.end("Unauthorized")
+		}
+	} else {
+		response.writeHead(404)
+		response.end("Not Found")
+	}
+	// 将HTTP响应200写入response, 同时设置Content-Type: text/html:
+	// 将HTTP响应的HTML内容写入response:
+})
+
+server.listen(1998)
